05b570eead58cbe4ab056e749e72a6f48fc45996
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2020-03-20 10:33:00 +0200

    HTTP POST request with body param (and urlencoded)

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const app = express();
 
+//public files (html, css, etc.)
 app.use(express.static('public'));
+//read the body of http POST/PUT/PATCH/DELETE request
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/test', (req, res) => {
+  //e.g. http://localhost:3000/test?demo=this%20is%20just%20a%20test&more=fun&extra=etc
   console.log('get request to /',req.path, req.query);
   res.send(`Hello there! demo URL param: ${req.query.demo} and more: ${req.query.more}`);
 });
@@ -20,7 +24,8 @@ app.get('/catinfo', (req, res) => {
 });
 
 app.post('/catinfo', (req, res) => {
-  res.send('catinfo with HTTP POST 😊');
+  console.log('HTTP POST with body params', req.body);
+  res.send(`catinfo with HTTP POST 😊 with name: ${req.body.name} and age: ${req.body.age}`);
 });
 
 app.listen(3000);
