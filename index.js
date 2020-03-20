3eb552ffae30188456c81378937278b322724052
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2020-03-20 10:06:39 +0200

    HTTP GET with url param after ?

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  console.log('get request to /', req);
-  res.send('Hello there!');
+app.get('/test', (req, res) => {
+  console.log('get request to /',req.path, req.query);
+  res.send(`Hello there! demo URL param: ${req.query.demo} and more: ${req.query.more}`);
 });
 
 app.get('/catinfo', (req, res) => {
@@ -16,6 +16,11 @@ app.get('/catinfo', (req, res) => {
     'age': 6,
     'weight': 5,
   }
+  res.json(cat);
+});
+
+app.post('/catinfo', (req, res) => {
+  res.send('catinfo with HTTP POST 😊');
 });
 
 app.listen(3000);
