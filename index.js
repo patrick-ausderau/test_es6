37207635b938227a710fce495eff6d519efc4304
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2020-03-20 09:46:57 +0200

    public static form

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   console.log('get request to /', req);
   res.send('Hello there!');
